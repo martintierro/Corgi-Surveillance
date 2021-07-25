aa57b8fed84327b38a4e6aa5c7c4d8aab424fdf6
Martin Tierro <martintierro15@gmail.com>
2021-07-25 15:50:45 +0800

    Added downsampling

--- background_subtraction.py
+++ background_subtraction.py
@@ -127,8 +127,10 @@ def background_subtraction(filename, video_name):
         
         frame_fg, frame_bg = perform_subtraction(frame, bg_plate, fgMask)
 
-
-        contours, hierarchy  = cv.findContours(fgMask, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
+        small_mask = cv.resize(fgMask, (frame_width//2,frame_height//2))
+        small_frame = cv.resize(frame, (frame_width//2,frame_height//2))
+        contours, hierarchy  = cv.findContours(small_mask, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
+        cv.imshow("Small Mask", small_mask)
         
         most_area = 0
         most_cnt = None
@@ -151,8 +153,10 @@ def background_subtraction(filename, video_name):
                 y = 1
             bgdModel = np.zeros((1,65),np.float64)
             fgdModel = np.zeros((1,65),np.float64)
-            cv.grabCut(frame,fgMask,(x,y,w,h),bgdModel,fgdModel,5,cv.GC_INIT_WITH_RECT)
-            fgMask = np.where((fgMask==2)|(fgMask==0),0,1).astype('uint8')
+            cv.grabCut(small_frame,small_mask,(x,y,w,h),bgdModel,fgdModel,5,cv.GC_INIT_WITH_RECT)
+            small_mask = np.where((small_mask==2)|(small_mask==0),0,1).astype('uint8')
+            fgMask = cv.resize(small_mask, (frame_width*2,frame_height*2), interpolation=cv.INTER_NEAREST)
+
         # cv.imshow("Bounding Box", frame)
         # cv.imshow("Bounding Box", frame)
         fgMask = fgMask * 255
